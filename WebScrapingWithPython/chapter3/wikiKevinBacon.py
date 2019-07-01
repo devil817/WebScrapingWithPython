a037d8d79699a9f4204c4cd172b5d4a0e3061bfc
devil817 <devil8172@gmail.com>
2019-07-01 23:15:53 +0900

    first commit

--- WebScrapingWithPython/chapter3/wikiKevinBacon.py
+++ WebScrapingWithPython/chapter3/wikiKevinBacon.py
@@ -2,19 +2,27 @@ from urllib.request import urlopen
 from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 import re
+import random
+import datetime
 
-try:
-    html = urlopen("http://en.wikipedia.org/wiki/kevin_Bacon")
-except HTTPError as e:
-    print(e)
+random.seed(datetime.datetime.now())
 
-try:
-    bsObj = BeautifulSoup(html, "html.parser")
-    links = bsObj.find("div", {"id": "bodyContent"}).findAll("a", href=re.compile("^/wiki/((?!:).)*$"))
-    for link in links:
-        if 'href' in link.attrs:
-            print(link.attrs['href'])
-except AttributeError as e:
-    print(e)
+def getLinks(url):
+    try:
+        html = urlopen("http://en.wikipedia.org"+url)
+    except HTTPError as e:
+        print(e)
+        return None
+    try:
+        bsObj = BeautifulSoup(html, "html.parser")
+        links = bsObj.find("div", {"id": "bodyContent"}).findAll("a", href=re.compile("^/wiki/((?!:).)*$"))
+        return links
+    except AttributeError as e:
+        print(e)
+        return None
 
-
+links = getLinks("/wiki/Kevin_Bacon")
+while len(links) > 0 :
+    newArticle = links[random.randint(0, len(links)-1)].attrs["href"]
+    print(newArticle)
+    links = getLinks(newArticle)
